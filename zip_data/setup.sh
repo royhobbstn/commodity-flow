f69b63b4ac89bb902aa8e10e39dfc252bcdfb6a8
Daniel Trone <danieljtrone@gmail.com>
2018-12-30 15:11:56 -0600

    create comprehensive zip lookup

--- zip_data/setup.sh
+++ zip_data/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://www2.census.gov/geo/tiger/GENZ2017/shp/cb_2017_us_zcta510_500k.zip
-unzip cb_2017_us_zcta510_500k.zip
-ogr2ogr simple_zip.shp cb_2017_us_zcta510_500k.shp -simplify 0.01
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2018/ZCTA5/tl_2018_us_zcta510.zip
+unzip tl_2018_us_zcta510.zip
+ogr2ogr simple_zip.shp tl_2018_us_zcta510.shp -simplify 0.01
 ogr2ogr -f GeoJSON -t_srs crs:84 zip.geojson simple_zip.shp
 
+node createZipCoordLookup.js
